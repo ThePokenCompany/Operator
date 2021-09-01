bd44dd3aac51456e4e23474bd8d1c5f60e680fe7
pokenDev <85866987+pokenDev@users.noreply.github.com>
2021-09-01 22:01:06 +0530

    Add tokenMover contract

--- src/contracts/Operator.sol
+++ src/contracts/Operator.sol
@@ -9,17 +9,45 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IERC2981Royalties.sol";
 import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
 
+contract TokenMover is Ownable {
+    mapping(address => bool) internal _isOperator;
+
+    modifier onlyOperator() {
+        require(_isOperator[_msgSender()], "Caller is not the operator");
+        _;
+    }
+    function addOperator(address _operator) public onlyOwner {
+        require(!_isOperator[_operator], "Address already added as operator");
+        _isOperator[_operator] = true;
+    }
+
+    function removeOperator(address _operator) public onlyOwner {
+        require(_isOperator[_operator], "Address is not added as operator");
+        _isOperator[_operator] = false;
+    }
+
+    function transferERC20(address currency, address from, address to, uint amount) public onlyOperator {
+        ERC20(currency).transferFrom(from, to, amount);
+    }
+
+    function transferERC721(address currency, address from, address to, uint tokenId) public onlyOperator {
+        ERC721(currency).safeTransferFrom(from, to, tokenId);
+    }
+}
+
 contract Operator is Ownable {
     address private feeManager;
     address private feeRecipient;
+    TokenMover public tokenMover;
 
     event SaleAwarded(address from, address to, uint tokenId);
     event ItemGifted(address from, address to, uint tokenId);
     event RoyaltyTransferred(address from, address to, uint amount);
 
-    constructor(address _feeManager, address _feeRecipient) {
+    constructor(address _feeManager, address _feeRecipient, address _TokenMover) {
         feeManager = _feeManager;
         feeRecipient = _feeRecipient;
+        tokenMover = TokenMover(_TokenMover);
     }
 
     function changeFeeManager(address _feeManager) public onlyOwner {
@@ -30,11 +58,11 @@ contract Operator is Ownable {
         feeRecipient = _feeRecipient;
     }
 
-    function getFeeManager() public view onlyOwner returns(address) {
+    function getFeeManager() public view returns(address) {
         return feeManager;
     }
 
-    function getFeeRecipient() public view onlyOwner returns(address) {
+    function getFeeRecipient() public view returns(address) {
         return feeRecipient;
     }
 
@@ -53,7 +81,7 @@ contract Operator is Ownable {
             if(owner != _receiver){
                 receiver = _receiver;
                 royaltyAmount = _royaltyAmount;
-            }            
+            }
         }
 
         uint commission = FeeManager(feeManager).getPartnerFee(owner);
@@ -61,22 +89,21 @@ contract Operator is Ownable {
         uint fee = price * commission /10000;
         uint amount = price - fee - royaltyAmount;
 
-        ERC20(currency).transferFrom(buyer, owner, amount);
-        ERC20(currency).transferFrom(buyer, feeRecipient, fee);
+        tokenMover.transferERC20(currency, buyer, owner, amount);
+        tokenMover.transferERC20(currency, buyer, feeRecipient, fee);
         if(receiver != address(0)){
-            ERC20(currency).transferFrom(buyer, receiver, royaltyAmount);
+            tokenMover.transferERC20(currency, buyer, receiver, royaltyAmount);
             emit RoyaltyTransferred(buyer, receiver, royaltyAmount);
         }
-        ERC721(nftContract).safeTransferFrom(owner, buyer, tokenId);
+        tokenMover.transferERC721(nftContract, owner, buyer, tokenId);
         emit SaleAwarded(owner, buyer, tokenId);
     }
 
-    // TODO: To be tested
     function giftItem(uint tokenId, address receiver, address nftContract, address owner) public onlyOwner {
         require(receiver != address(0), "receiver cannot be the zero address");
         require(owner != address(0), "Owner cannot be the zero address");
 
-        ERC721(nftContract).safeTransferFrom(owner, receiver, tokenId);
+        tokenMover.transferERC721(nftContract, owner, receiver, tokenId);
         emit ItemGifted(owner, receiver, tokenId);
     }
 }
